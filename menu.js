062a72469349b7d26cf1fcac769f4ecb63810ab4
Frank Fuhrmann <frank@ff-sec.eu>
2021-08-26 18:38:21 +0200

    sounds library added

--- menu.js
+++ menu.js
@@ -1,2 +1,2 @@
 
-document.write('<div class="menu"><a href="index.html">Start</a> - <a href="library.html">Bibliothek</a><br /><br /></div>');
+document.write('<div class="menu"><a href="index.html">Start</a> - <a href="library.html">Bibliothek</a> - <a href="sounds.html">Vertontes</a><br /><br /></div>');
