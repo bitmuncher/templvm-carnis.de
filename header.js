fcd7d28a14403880adfd7426740307e6d248ab29
Frank Fuhrmann <frank@ff-sec.eu>
2021-08-27 23:30:12 +0200

    new header dependent on mobile or desktop browser

--- header.js
+++ header.js
@@ -1,11 +1,21 @@
-document.write('\
+if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
+    document.write('\
+<header>\
+    <center>\
+        <h1>&nbsp;TEMPLVM CARNIS&nbsp;</h1>\
+    </center>\
+</header>\
+    ');
+} else {
+    document.write('\
 <header>\
     <center>\
         <h1> \
-            <img src="data/images/templvm_carnis_logo_dark_50.jpg" />\
+            <img src="data/images/templvm_carnis_logo_dark_100.jpg" style="float:left"/>\
             &nbsp;TEMPLVM CARNIS&nbsp;\
-            <img src="data/images/templvm_carnis_logo_dark_50.jpg" />\
+            <img src="data/images/templvm_carnis_logo_dark_100.jpg" style="float:right"/>\
         </h1>\
     </center>\
 </header>\
-');
+    ');
+}
