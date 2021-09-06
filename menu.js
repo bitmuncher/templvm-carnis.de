ed023badd61b076d6cd3910f3a66c4c674c12c8a
Frank Fuhrmann <frank@ff-sec.eu>
2021-09-07 01:44:57 +0200

    Sound-Sektion umbenannt

--- menu.js
+++ menu.js
@@ -2,7 +2,7 @@ document.write('<div class="menu-container">\
 <ul class="menu" id="menu">\
 <li class="menu-item"><a href="index.html">Start</a></li>\
 <li class="menu-item"><a href="library.html">Bibliothek</a></li>\
-<li class="menu-item"><a href="sounds.html">Vertontes</a></li>\
+<li class="menu-item"><a href="sounds.html">Klang-Garten</a></li>\
 <li class="menu-item"><a href="about.html">Über den TC</a></li>\
 </ul>\
 </div>\
