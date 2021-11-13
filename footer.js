52917e2b54d5ec347d2225e4df3f269de37c9426
Theoten <theoten@templvm-carnis.de>
2021-11-13 02:04:15 +0100

    better footer

--- footer.js
+++ footer.js
@@ -1,1 +1,1 @@
-document.write('<footer><br />&copy; 2021 Templvm Carnis - Contact: info@templvm-carnis.de<br /></footer>');
+document.write('<footer><br />&copy; 2021 Templvm Carnis - Contact: info@templvm-carnis.de<br /></footer><br />');
