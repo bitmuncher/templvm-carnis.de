c503c4028d6b7f6882ca4c5ef76f17343a27f58f
Theoten <theoten@templvm-carnis.de>
2022-06-15 23:44:35 +0200

    updated year in copyright info

--- footer.js
+++ footer.js
@@ -1,1 +1,1 @@
-document.write('<footer><br />&copy; 2021 Templvm Carnis - Contact: info@templvm-carnis.de<br /></footer><br />');
+document.write('<footer><br />&copy; 2022 Templvm Carnis - Contact: info@templvm-carnis.de<br /></footer><br />');
