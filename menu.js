9d8cf61eebba5e8f64aeb02e5fd085581e063ce3
Frank Fuhrmann <frank@ff-sec.eu>
2022-10-05 18:13:45 +0200

    fixed german special char

--- menu.js
+++ menu.js
@@ -1,9 +1,9 @@
 document.write('<div class="menu-container">\
 <ul class="menu" id="menu">\
-<li class="menu-item"><a href="/index.html">Start</a></li>\
-<li class="menu-item"><a href="/library.html">Bibliothek</a></li>\
-<li class="menu-item"><a href="/sounds.html">Klang-Garten</a></li>\
-<li class="menu-item"><a href="/about.html">Über den TC</a></li>\
+<li class="menu-item"><a href="index.html">Start</a></li>\
+<li class="menu-item"><a href="library.html">Bibliothek</a></li>\
+<li class="menu-item"><a href="sounds.html">Klang-Garten</a></li>\
+<li class="menu-item"><a href="about.html">&Uuml;ber den TC</a></li>\
 </ul>\
 </div>\
 <br />\
